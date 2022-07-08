94502d6c81de6ca6fdc52b4a7550666e4cee6565
Sangmin <besforyou2@gmail.com>
2022-07-08 16:06:25 +0900

    change graph layout to vertical

--- korea_crime_graph/src/components/Chart.js
+++ korea_crime_graph/src/components/Chart.js
@@ -4,6 +4,7 @@ import {
 } from 'recharts';
 
 function create_data(crime_name, case_count) {
+  console.log(crime_name, case_count);
   return {
     name: crime_name,
     case: case_count
@@ -17,7 +18,8 @@ export default class Chart extends PureComponent {
     const locations = this.props.locations;
     const object = this.props.obj;
 
-    locations.shift();
+    if (locations.length >= 17)
+      locations.shift();
 
     locations.map( element => (
       new_data.push(create_data(element, object[element]))
@@ -30,22 +32,21 @@ export default class Chart extends PureComponent {
 
   render() {
     return (
-      <div>
-        <ResponsiveContainer width="95%" height={700}>
+        <ResponsiveContainer width="95%" height="90%">
           <BarChart data={this.state.data}
             margin={{
-              top: 40, right: 30, left: 0, bottom: 5,
+              top: 0, right: 30, left: 0, bottom: 5,
             }}
+            layout="vertical"
           >
             <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="name" />
-            <YAxis />
+            <XAxis type="number"/>
+            <YAxis dataKey="name" type="category"/>
             <Tooltip />
             <Legend />
             <Bar dataKey="case" fill="gray" />
           </BarChart>
         </ResponsiveContainer>
-      </div>
     );
   }
 }

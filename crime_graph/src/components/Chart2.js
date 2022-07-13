73ca689abea94afa1817630e70af02b9b01de581
Sangmin <besforyou2@gmail.com>
2022-07-13 15:34:57 +0900

    delete unused files

--- crime_graph/src/components/Chart2.js
+++ crime_graph/src/components/Chart2.js
@@ -2,11 +2,32 @@ import React from 'react';
 import {
   XAxis, YAxis, CartesianGrid, Tooltip, Legend, BarChart, Bar, ResponsiveContainer
 } from 'recharts';
+import { useHistory } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 
 export default function Chart2(props) {
+  const history = useHistory();
+  const csvData = useSelector(state => state.csvData.csvData).data;
+
+  function handleClick(name) {
+    let obj;
+    for (let i = 0 ; i < csvData.length ; i++) {
+      if (name === csvData[i].범죄분류) {
+        obj = csvData[i];
+        break;
+      }
+    }
+    history.push({
+      pathname: '/CrimeSpec',
+      state: {
+        object: obj
+      }
+    })
+  }
+
   return (
-      <ResponsiveContainer width="100%" height="100%">
+      <ResponsiveContainer width="95%" height="95%">
         <BarChart data={props.data}
           margin={{
             top: 20, right: 10, left: 20, bottom: 5,
@@ -18,7 +39,9 @@ export default function Chart2(props) {
           <YAxis dataKey="name" type="category"/>
           <Tooltip />
           <Legend />
-          <Bar dataKey="number" fill="gray" />
+          <Bar dataKey="number" fill="gray" onClick={ function(e) {
+            handleClick(e.name);
+          }}/>
         </BarChart>
       </ResponsiveContainer>
   );

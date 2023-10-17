b9ba8a90fe326d606dbbfad471292290096bb694
주상민 <besforyou2@gmail.com>
2023-10-17 22:27:04 +0900

    feat: env 파일에 api key 숨김

--- src/containers/App.js
+++ src/containers/App.js
@@ -6,6 +6,7 @@ import Papa from 'papaparse';
 import { useDispatch } from 'react-redux';
 import { setCsvData , setLocations } from '../features/csvData/csvDataSlice.js';
 
+
 function App () { 
 
   const [isLoading, setLoading] = useState(true);
@@ -20,6 +21,8 @@ function App () {
       complete: results => {
         const keys = Object.keys(results.data[0]);
         keys.splice(0,1);
+        // console.log(keys);
+        // console.log(results);
         dispatch(setLocations(keys));
         dispatch(setCsvData(results));
         setLoading(false); // 파싱 성공 -> 로딩 종료
@@ -28,8 +31,17 @@ function App () {
     });
   }
 
+  const fetch_data = async () => {
+    const api_key = process.env.REACT_APP_APIKEY;
+    const url = `https://api.odcloud.kr/api/15085727/v1/uddi:d57791f7-1e1e-46c9-bbfd-911fa64ee8a4?page=1&perPage=200&serviceKey=${api_key}&dataType=JSON`;
+    const result = await fetch(url);
+    const json = await result.json();
+    console.log(json);
+  }
+
   useEffect(()=>{
     parseCsvFile();
+    fetch_data();
   })
 
   return (
--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import ScrollToTop from './containers/ScrollToTop';
 import store from './app/store';
 import { Provider } from 'react-redux';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <Provider store={store}>

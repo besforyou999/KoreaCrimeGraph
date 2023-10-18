581db8ecf8ab86774e2bc1188af49e074371f4d4
주상민 <besforyou2@gmail.com>
2023-10-18 12:46:06 +0900

    feat: papaparse로 fetch 실패 대비
    fetch로 데이터 읽어오는 과정이 실패할 경우에 대비해
    프로젝트에 존재하는 데이터를 파싱해 사용하는 코드 추가
    
    네트워크 통신 실패로 데이터 읽어오지 못하는 경우 대비 가능
    단점으론, csv 파일 크기만큼 프로젝트 크기가 커진다. (약 10KB)

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,7 @@
 import React, {useState } from "react";
 import MainPage from "../containers/MainPage.js";
+import csvFile from '../resource/crime_loc.csv';
+import Papa from 'papaparse';
 
 import { useDispatch } from 'react-redux';
 import { setCsvData , setLocations } from '../features/csvData/csvDataSlice.js';
@@ -11,13 +13,8 @@ function App () {
   const [dataFetched, setDataFetched] = useState(false);
   const dispatch = useDispatch();
 
-  const fetch_data = async () => {
-    console.log("fetching data with api")
-    const api_key = process.env.REACT_APP_APIKEY;
-    const url = `https://api.odcloud.kr/api/15085727/v1/uddi:d57791f7-1e1e-46c9-bbfd-911fa64ee8a4?page=1&perPage=200&serviceKey=${api_key}&dataType=JSON`;
-    const result = await fetch(url);
+  const fetch_success = async result => {
     const json = await result.json();
-
     // 전체 데이터 store에 전송
     dispatch(setCsvData(json));
 
@@ -30,8 +27,38 @@ function App () {
     setDataFetched(true);
   }
 
-  if (isLoading && !dataFetched) 
-    fetch_data();
+  const fetch_fail = () => {
+    Papa.parse(csvFile, {
+      download: true,
+      header: true,
+      dynamicTyping: true,
+      complete: results => {
+        const keys = Object.keys(results.data[0]);
+        keys.splice(0,1); // 키값 중 '범죄분류' 제거
+        dispatch(setLocations(keys));
+        dispatch(setCsvData(results));
+        setLoading(false);
+        setDataFetched(true);
+      },
+      error: () => console.log("csv 파일 파싱 에러"),
+    });
+  }
+
+  const fetch_data = async () => {
+    const api_key = process.env.REACT_APP_APIKEY;
+    const url = `https://api.odcloud.kr/api/15085727/v1/uddi:d57791f7-1e1e-46c9-bbfd-911fa64ee8a4?page=1&perPage=200&serviceKey=${api_key}&dataType=JSON`;
+    const result = await fetch(url);
+
+    if (result.status === 200) {
+      console.log("success");
+      fetch_success(result);
+    } else {
+      console.log("fail")
+      fetch_fail(result);
+    }
+  }
+
+  if (isLoading && !dataFetched) fetch_data();
 
   return (
     <div className="container">

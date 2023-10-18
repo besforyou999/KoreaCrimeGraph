feef196e4334147aca37b72e12afaebe245e1698
주상민 <besforyou2@gmail.com>
2023-10-18 12:01:54 +0900

    feat: api로 범죄 데이터 가져오기

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,5 @@
-import React, {useState, useEffect} from "react";
+import React, {useState } from "react";
 import MainPage from "../containers/MainPage.js";
-import csvFile from '../resource/crime_loc.csv';
-import Papa from 'papaparse';
 
 import { useDispatch } from 'react-redux';
 import { setCsvData , setLocations } from '../features/csvData/csvDataSlice.js';
@@ -10,39 +8,30 @@ import { setCsvData , setLocations } from '../features/csvData/csvDataSlice.js';
 function App () { 
 
   const [isLoading, setLoading] = useState(true);
+  const [dataFetched, setDataFetched] = useState(false);
   const dispatch = useDispatch();
 
-  // 범죄데이터 csv 파일 파싱 함수
-  function parseCsvFile() {
-    Papa.parse(csvFile, {
-      download: true,
-      header: true,
-      dynamicTyping: true,
-      complete: results => {
-        const keys = Object.keys(results.data[0]);
-        keys.splice(0,1);
-        // console.log(keys);
-        // console.log(results);
-        dispatch(setLocations(keys));
-        dispatch(setCsvData(results));
-        setLoading(false); // 파싱 성공 -> 로딩 종료
-      },
-      error: () => console.log("error while parsing csv"),
-    });
-  }
-
   const fetch_data = async () => {
+    console.log("fetching data with api")
     const api_key = process.env.REACT_APP_APIKEY;
     const url = `https://api.odcloud.kr/api/15085727/v1/uddi:d57791f7-1e1e-46c9-bbfd-911fa64ee8a4?page=1&perPage=200&serviceKey=${api_key}&dataType=JSON`;
     const result = await fetch(url);
     const json = await result.json();
-    console.log(json);
+
+    // 전체 데이터 store에 전송
+    dispatch(setCsvData(json));
+
+    // 지역 데이터 store에 전송
+    const locations = Object.keys(json.data[0]);
+    locations.splice(7, 1); // 중간에 '범죄분류' 키 삭제
+    dispatch(setLocations(locations))
+
+    setLoading(false);
+    setDataFetched(true);
   }
 
-  useEffect(()=>{
-    parseCsvFile();
+  if (isLoading && !dataFetched) 
     fetch_data();
-  })
 
   return (
     <div className="container">

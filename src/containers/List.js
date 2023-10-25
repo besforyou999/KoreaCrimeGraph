272d457d98ac28bae63a2a33b78ce7c6593b0d74
주상민 <besforyou2@gmail.com>
2023-10-25 22:47:51 +0900

    feat: 한글 초성 검색
    출처 : https://gurtn.tistory.com/209

--- src/containers/List.js
+++ src/containers/List.js
@@ -1,8 +1,10 @@
-import React, { useMemo } from 'react'
+import React from 'react';
 import Input from '../presentational/List/Input.js';
 import Board from '../presentational/List/Board.js';
 
 import { useSelector} from 'react-redux';
+
+import includeByCho from '../hangul_search/includeByCho.js';
 
 function List() {
 
@@ -10,11 +12,11 @@ function List() {
   const crime_count = useSelector(state => state.csvData.crime_count);
 
   const crime_arr = []
-  for (const key in crime_count) 
-    crime_arr.push([key, crime_count[key]])
-
+  for (const key in crime_count) {
+    if (includeByCho(input, key))
+      crime_arr.push([key, crime_count[key]])
+  }
   
-
   return (
     <div>
       <Input/>
--- src/presentational/List/Board.js
+++ src/presentational/List/Board.js
@@ -2,11 +2,11 @@ import React from 'react';
 import BoardItem from './BoardItem.js';
 
 function Board({data}) {
-  
+
   return (
     <div className='board'>
       {data.map(element => (
-        <BoardItem name={element[0]} number={element[1]}/>
+        <BoardItem name={element[0]} number={element[1]} key={element[0]}/>
       ))}
     </div>
   )

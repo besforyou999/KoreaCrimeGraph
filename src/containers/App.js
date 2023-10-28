1f0b2a1f8b5f3fbc39d5cbc2cbd774599e9b533e
주상민 <besforyou2@gmail.com>
2023-10-28 14:48:47 +0900

    fix: useEffect로 변경

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import MainPage from "../containers/MainPage.js";
 import csvFile from '../resource/crime_loc_2019.csv';
 import Papa from 'papaparse';
@@ -51,22 +51,23 @@ function App () {
     });
   }
 
-  const fetch_data = async () => {
-    const api_key = process.env.REACT_APP_APIKEY;
-    const url = `https://api.odcloud.kr/api/15085727/v1/uddi:d57791f7-1e1e-46c9-bbfd-911fa64ee8a4?page=1&perPage=200&serviceKey=${api_key}&dataType=JSON`;
-
-    try {
-      const result = await fetch(url)
-      fetch_success(result)
-    } catch (err) {
-      console.log(err);
-      console.log("데이터 불러오기 실패. csv 데이터 파일 파싱 실행")
-      fetch_fail()
+  useEffect(() => {
+    async function fetch_data() {
+      console.log("fetch_data")
+      console.log("dataRead : ", dataRead)
+      const api_key = process.env.REACT_APP_APIKEY;
+      const url = `https://api.odcloud.kr/api/15085727/v1/uddi:d57791f7-1e1e-46c9-bbfd-911fa64ee8a4?page=1&perPage=200&serviceKey=${api_key}&dataType=JSON`;
+      try {
+        const result = await fetch(url)
+        fetch_success(result)
+      } catch (err) {
+        console.log(err);
+        console.log("데이터 불러오기 실패. csv 데이터 파일 파싱 실행")
+        fetch_fail()
+      }
     }
-  }
-
-  if (!dataRead)
-    fetch_data();
+    fetch_data()
+  }, [])
 
   return (
     <div className="container" id='container'>
